8a52e47e127c540427ad4f6f17ce50300eadbfde
adillahfatih <90895221+adillahfatih@users.noreply.github.com>
2021-09-19 13:43:09 +0700

    Program Latihan Pertama.cpp

---
+++ Program Latihan Pertama.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	system("color c");
+	system("Title Program Pertama");
+	
+	int b;
+	cout<<"Ini Adalah Program Latihan Saya, Harap Dimaklumi jika Belum Seberapa"<<endl;
+	cout<<"Halo Saya Fatih, Saya Akan Mengetahui Umur Anda"<<endl;
+	cout<<"Silahkan Masukkan Tahun Lahir Anda : ";
+	cin>>b;
+	cout<<"Umur Anda Anda Adalah : ";
+	cout<<2021-b<<endl;
+	
+	cin.get();
+	return 0;
+}
